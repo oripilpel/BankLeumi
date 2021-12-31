aa71d9842c0e277adcbe8130d66be717d9a80042
ori <oripilpelphone@gmail.com>
2021-12-31 19:00:11 +0200

    finished ,maybe try getForecastByDay with reduce

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const axios = require('axios');
 const ObjectsToCsv = require('objects-to-csv')
 
 function getCitiesWeather() {
-    // const cities = ['Jerusalem', 'New York', 'Dubai', 'Lisbon', 'Oslo', 'Paris', 'Berlin', 'Athens', 'Seoul', 'Singapore']
-    const cities = ['Jerusalem', 'New York']
+    const cities = ['Jerusalem', 'New York', 'Dubai', 'Lisbon', 'Oslo', 'Paris', 'Berlin', 'Athens', 'Seoul', 'Singapore']
     const latLonPrms = cities.map(cityName => getCityLatLon(cityName))
     Promise.all(latLonPrms).then(latLons => {
         const citiesWeatherPrms = latLons.map(latLon => getCityWeather(latLon))
@@ -13,7 +12,7 @@ function getCitiesWeather() {
             .then(citiesWeather => {
                 citiesWeather = citiesWeather.map(cityPrm => getFormattedDailyWeather(cityPrm))
                 const citiesByDay = getForcastByDay(citiesWeather)
-                writeToCsv(citiesWeather)
+                // writeToCsv(citiesWeather)
                 writeToCsv(citiesByDay)
             })
     })
@@ -35,27 +34,28 @@ function getFormattedDailyWeather({ data }) {
         daily => ({
             name: getCityName(data.timezone),
             date: daily.dt * 1000,
-            temp: { min: daily.temp.min, max: daily.temp.max },
+            temp: { min: daily.temp.min, max: daily.temp.max,day:daily.temp.day },
             isRain: daily.weather.some(w => w.main === 'Rain')
         })
     )
 }
 
 function getForcastByDay(cities) {
-    debugger
     if (!cities[0]) return
     const days = cities[0].map((d) => ({
-        date: getDayName(d.date),
+        date: getFormattedDate(d.date),
         maxTempCity: { temp: d.temp.max, name: d.name },
         minTempCity: { temp: d.temp.min, name: d.name },
-        isRain: []
+        citiesForecast:[],
+        citiesWithRain: []
     }))
     cities.forEach(city => {
         city.forEach(({ temp, isRain, name }, idx) => {
             const currDay = days[idx]
+            currDay.citiesForecast.push({name,temp:temp.day})
             if (temp.max > currDay.maxTempCity.temp) currDay.maxTempCity = { temp: temp.max, name }
             if (temp.min < currDay.minTempCity.temp) currDay.minTempCity = { temp: temp.min, name }
-            if (isRain) currDay.isRain.push(name)
+            if (isRain) currDay.citiesWithRain.push(name)
         })
     })
     return days
@@ -66,10 +66,8 @@ function getCityName(timezone) {
     return timezone.slice(idx + 1).replace('_', ' ')
 }
 
-function getDayName(timestamp) {
-    const idx = new Date(timestamp).getDay()
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    return days[idx]
+function getFormattedDate(timestamp) {
+  return new Date(timestamp).toLocaleString('en-US',{weekday:'long',day:'numeric',month:'numeric',year:'numeric'})
 }
 
 async function writeToCsv(cities) {
@@ -77,14 +75,4 @@ async function writeToCsv(cities) {
     await csv.toDisk('./list.csv')
 }
 
-
-
-
 getCitiesWeather()
-
-// const express = require('express')
-
-// const app = express()
-// const http = require('http').createServer(app)
-
-// http.listen(port, () => { console.log('port', port) })
